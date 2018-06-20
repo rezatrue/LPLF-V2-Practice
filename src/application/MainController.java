80f10679d0a8ff3e267ccb12cb1638e376890fb2
rezatrue <rezatrue@yahoo.com>
2018-06-20 15:42:50 +0600

    Powered by logo added

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,24 +1,35 @@
 package application;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController {
 	
 	@FXML
 	private Button btnLaunch, btnLogin, btnSettings, btnBrowse, btnRun; 
 	
+	@FXML
+	private ImageView logoView;
 
 	// constructor is called before initialize() method
 	public MainController() {
 		System.out.println("Constructor");
-
+		
+		
 	}
 	
 	@FXML
 	private void initialize() {
 		System.out.println("Initialize");
+		File file = new File("image/yin-yang.jpg");
+        Image image = new Image(file.toURI().toString());
+        logoView.setImage(image);
+		
 	}
 	
 	@FXML
