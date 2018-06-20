c08082fc96418fb5d6931d2ea649aa2adfc6603d
rezatrue <rezatrue@yahoo.com>
2018-06-20 14:30:30 +0600

    Button Action implemented

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,51 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainController {
+	
+	@FXML
+	private Button btnLaunch, btnLogin, btnSettings, btnBrowse, btnRun; 
+	
+
+	// constructor is called before initialize() method
+	public MainController() {
+		System.out.println("Constructor");
+
+	}
+	
+	@FXML
+	private void initialize() {
+		System.out.println("Initialize");
+	}
+	
+	@FXML
+	private void launchBtnAction(ActionEvent event) {
+		System.out.println("Launch Button");
+	}
+	
+	@FXML
+	private void loginBtnAction(ActionEvent event) {
+		System.out.println("Login Button");
+	}
+	
+	@FXML
+	private void settingsBtnAction(ActionEvent event) {
+		System.out.println("Settings Button");
+	}
+	
+	@FXML
+	private void browseBtnAction(ActionEvent event) {
+		System.out.println("Browse Button");
+	}
+	
+	@FXML
+	private void runBtnAction(ActionEvent event) {
+		System.out.println("Run Button");
+	}
+
+	
 	
 }
