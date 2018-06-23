2b352186e70d5a2a0362541226062f190cb905df
rezatrue <rezatrue@gmail.com>
2018-06-23 08:35:35 +0600

    login dialogue implemented

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -3,23 +3,33 @@ package application;
 import java.io.File;
 import java.net.URL;
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import scrapper.CsvFileHandeler;
 import scrapper.FireFoxOperator;
 import scrapper.Info;
@@ -53,7 +63,7 @@ public class MainController implements Initializable{
 		System.out.println("Initialize");
 		csvFileHandeler = new CsvFileHandeler();
 		fireFoxOperator = new FireFoxOperator();
-		
+		btnLaunch.setDisable(true);
 		btnLogin.setDisable(true);
 		btnBrowse.setDisable(true);
 		btnRun.setDisable(true);
@@ -78,6 +88,8 @@ public class MainController implements Initializable{
     	        }
     	    }
     	});
+        
+        loginDialoag();
 		
 	}
 	
@@ -172,5 +184,81 @@ public class MainController implements Initializable{
 		csvFileHandeler.write(list, list.size()+"");
 	}
 	
+	// source http://code.makery.ch/blog/javafx-dialogs-official/
+	// just copy pest
+
+	private void loginDialoag() {
+		//prefs = Preferences.userRoot().node("db");
+		//dBHandler = new DBHandler();
+		String msg = "";
+
+		// Create the custom dialog.
+		Dialog<Pair<String, String>> dialog = new Dialog<>();
+		dialog.setTitle("Login");
+		dialog.setHeaderText("Please Enter your usernmae & password");
+
+		// Set the icon (must be included in the project).
+        //dialog.setGraphic(new ImageView(this.getClass().getResource("image/login.png").toString()));
+		
+		
+
+		// Set the button types.
+		ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+		// Create the username and password labels and fields.
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(20, 150, 10, 10));
+
+		TextField username = new TextField();
+		username.setPromptText("Username");
+		//username.setText(prefs.get("user", ""));
+		PasswordField password = new PasswordField();
+		password.setPromptText("Password");
+		//password.setText(prefs.get("password", ""));
+
+		grid.add(new Label("Username:"), 0, 0);
+		grid.add(username, 1, 0);
+		grid.add(new Label("Password:"), 0, 1);
+		grid.add(password, 1, 1);
+
+		// Enable/Disable login button depending on whether a username was
+		// entered.
+		Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+		loginButton.setDisable(true);
+
+		// Do some validation (using the Java 8 lambda syntax).
+		username.textProperty().addListener((observable, oldValue, newValue) -> {
+			loginButton.setDisable(newValue.trim().isEmpty());
+		});
+
+		dialog.getDialogPane().setContent(grid);
+
+		// Request focus on the username field by default.
+		Platform.runLater(() -> username.requestFocus());
+
+		// Convert the result to a username-password-pair when the login button
+		// is clicked.
+		dialog.setResultConverter(dialogButton -> {
+			if (dialogButton == loginButtonType) {
+				return new Pair<>(username.getText(), password.getText());
+			}
+			return null;
+		});
+
+		Optional<Pair<String, String>> result = dialog.showAndWait();
+
+		result.ifPresent(usernamePassword -> {
+			System.out.println("Username=" + usernamePassword.getKey() + ", Password=" + usernamePassword.getValue());
+			// check auth 
+			//dBHandler.userAuth(usernamePassword.getKey(), usernamePassword.getValue());
+			if(true)btnLaunch.setDisable(false);
+
+		});
+
+
+	}
 	
 }
