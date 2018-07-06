e781b7cfb95ca6a8a035b24c6f4695f4e965c9bc
rezatrue <rezatrue@gmail.com>
2018-07-07 05:08:37 +0600

    TestMain Class ready to use

--- src/scrapper/TestMain.java
+++ src/scrapper/TestMain.java
@@ -4,6 +4,15 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		
+	FireFoxOperator fireFoxOperator = new FireFoxOperator();
+	
+	fireFoxOperator.browserLauncher();
+	fireFoxOperator.openUrl("https://www.linkedin.com/");
+	
+	fireFoxOperator.linkedinLogin("", "");
+	
+	fireFoxOperator.getPublicLink("");
+
 
 	}
 
